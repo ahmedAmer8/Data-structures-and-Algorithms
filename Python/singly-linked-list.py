6ec475aeb2a7405d5a8d8555c510f0e3acdaf9b8
Ahmed Amer <147430421+ahmedAmer8@users.noreply.github.com>
2024-05-19 18:11:44 +0300

    Update singly-linked-list.py

--- Python/singly-linked-list.py
+++ Python/singly-linked-list.py
@@ -1,5 +1,5 @@
 class Node:
-    def __init__(self, data=0):
+    def __init__(self, data=None):
         self.data = data
         self.next = None
 
@@ -90,7 +90,7 @@ class LinkedList:
     def print(self):
         current_node = self.head
         while current_node:
-            print(current_node.data)
+            print(current_node.data, end= " ")
             current_node = current_node.next
 
 
